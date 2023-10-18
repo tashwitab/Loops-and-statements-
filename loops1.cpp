45ac85fb7bf04d5b9abe1f799c224ab85566a405
Tashwita Bhirud <142425346+tashwitab@users.noreply.github.com>
2023-10-18 09:26:43 +0530

    Add files via upload

---
+++ loops1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the 3 numbers you want to compare: ";
+    cin>>a>>b>>c;
+    if(a>b && a>c){ //Compares whether the value of a is greater than b and c
+        cout<<a<<" is the greatest";
+    }
+    else if(b>c && b>a){ //Compares whether the value of b is greater than b and c
+        cout<<b<<" b is the greatest"<<b;
+    }
+    else{ //If both the conditions fail then this statement is executed.
+        cout<<c<<" is the greatest"<<c;
+    }
+    return 0;
+}
